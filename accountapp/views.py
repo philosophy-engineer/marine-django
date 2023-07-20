fe6a05d95c8e82d81c6ee66dc83e69bcf3ecbd6a
philosopher <philosopher@marine.com>
2023-07-20 13:38:23 +0000

    first marine django

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 def hello_world(request):
-    return HttpResponse('Hello world!')
+    return render(request, 'base.html')
