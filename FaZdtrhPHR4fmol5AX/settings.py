d749aa424efa2a787e59c039ce18ff2b7581f735
philosopher <philosopher@marine.com>
2023-07-30 11:41:57 +0000

    Django Wrap-up

--- FaZdtrhPHR4fmol5AX/settings.py
+++ FaZdtrhPHR4fmol5AX/settings.py
@@ -146,7 +146,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello-world')
+LOGIN_REDIRECT_URL = reverse_lazy('home')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
 
 
